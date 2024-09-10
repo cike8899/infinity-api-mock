1e22fa4f0b6d66d8c54c0534d01825cbf6454a59
bill <yibie_jingnian@163.com>
2024-09-10 16:16:32 +0800

    feat: Get products

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { data } from './products';
 
 @Injectable()
 export class AppService {
   getHello(): string {
     return 'Hello World!';
+  }
+  getProducts(): any[] {
+    return data.products;
   }
 }
---
+++ src/products.ts
@@ -0,0 +1,54 @@
+export const data = {
+  products: [
+    {
+      id: 1,
+      imageUrl:
+        'https://uwja77bygk2kgfqe.public.blob.vercel-storage.com/smartphone-gaPvyZW6aww0IhD3dOpaU6gBGILtcJ.webp',
+      name: 'Smartphone X Pro',
+      status: 'active',
+      price: '999.00',
+      stock: 150,
+      availableAt: '2024-09-03T06:12:18.163Z',
+    },
+    {
+      id: 2,
+      imageUrl:
+        'https://uwja77bygk2kgfqe.public.blob.vercel-storage.com/earbuds-3rew4JGdIK81KNlR8Edr8NBBhFTOtX.webp',
+      name: 'Wireless Earbuds Ultra',
+      status: 'active',
+      price: '199.00',
+      stock: 300,
+      availableAt: '2024-09-03T06:12:18.163Z',
+    },
+    {
+      id: 3,
+      imageUrl:
+        'https://uwja77bygk2kgfqe.public.blob.vercel-storage.com/home-iTeNnmKSMnrykOS9IYyJvnLFgap7Vw.webp',
+      name: 'Smart Home Hub',
+      status: 'active',
+      price: '149.00',
+      stock: 200,
+      availableAt: '2024-09-03T06:12:18.163Z',
+    },
+    {
+      id: 4,
+      imageUrl:
+        'https://uwja77bygk2kgfqe.public.blob.vercel-storage.com/tv-H4l26crxtm9EQHLWc0ddrsXZ0V0Ofw.webp',
+      name: '4K Ultra HD Smart TV',
+      status: 'active',
+      price: '799.00',
+      stock: 50,
+      availableAt: '2024-09-03T06:12:18.163Z',
+    },
+    {
+      id: 5,
+      imageUrl:
+        'https://uwja77bygk2kgfqe.public.blob.vercel-storage.com/laptop-9bgUhjY491hkxiMDeSgqb9R5I3lHNL.webp',
+      name: 'Gaming Laptop Pro',
+      status: 'active',
+      price: '1299.00',
+      stock: 75,
+      availableAt: '2024-09-03T06:12:18.163Z',
+    },
+  ],
+};
